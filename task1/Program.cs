eaef617b94100da9d74b0c6338df9fc462257db8
Alex <newalexis777@gmail.com>
2022-08-28 15:30:45 +0400

    task1 and task3

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задаем двумерный массив вещественных чисел.
+
+double[,] matr = new double[4, 3];
+
+Random random = new Random();
+for (int i = 0; i < 4; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        matr[i, j] = random.Next(-100, 100) + random.NextDouble();
+        Console.Write("{0,6:F2} ", matr[i, j]);
+    }
+    Console.WriteLine();
+}
+
+
+
+
+
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,50 @@
+﻿//В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+Console.WriteLine("Введите количество элементов в i: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество элементов в j: ");
+int y = int.Parse(Console.ReadLine());
+
+int [,] array = new int[x, y];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,20);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Kvadrat(int[,] matr2)
+{
+    for (int i = 0; i < matr2.GetLength(0); )
+    {
+        for (int j = 0; j < matr2.GetLength(1); )
+        {
+            matr2[i, j] = matr2[i, j] * matr2[i, j];
+            j = j + 2;
+        }
+        i = i + 2;
+    }
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Kvadrat(array);
+PrintArray(array);
