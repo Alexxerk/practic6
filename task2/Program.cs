2c50a7e850981b6d02f8ba9a3251db8d1d301b30
Alex <newalexis777@gmail.com>
2022-08-28 15:46:26 +0400

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿//Последовательная сумма индексов.
+
+int[,] matrix = new int[6, 6];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = i+j;
+        Console.Write(matrix[i, j] + " ");
+    }
+Console.WriteLine();
+}
